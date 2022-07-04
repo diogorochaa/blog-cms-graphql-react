d3f39132d0ff95615a3027c0ec4af4554e325773
DiogoRocha10 <diogorocha12@live.com>
2022-07-04 13:29:25 -0300

    feat/subscribers: adicionando pagina inicial

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4r135320hni01z65t5bhkuq/master",
+  uri: import.meta.env.VITE_API_URL,
   cache: new InMemoryCache(),
 });
